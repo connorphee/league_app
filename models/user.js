0d92870df658748bf0af5dbf579d84c4db79299d
wldn <wildansnahar@gmail.com>
2017-10-06 04:31:31 +0700

    activate link

--- models/user.js
+++ models/user.js
@@ -4,7 +4,9 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String,
-    email: String
+    email: String,
+    reset_link: String,
+    reset_pass: String
 });
 
 UserSchema.plugin(passportLocalMongoose)
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,16 @@ var router  = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 
+// emailjs configuration
+// TODO: separate config file
+const email= require('emailjs/email'),
+server = email.server.connect({
+    user: 'winistein@gmail.com',
+    password: 'W1LD4N5N',
+    host: 'smtp.gmail.com',
+    ssl: true
+});
+
 //root route
 router.get("/", function(req, res){
     if (req.user) {
@@ -27,18 +37,77 @@ router.post("/reset", function(req, res){
         email     = req.body.email
   // check if username && email exist
   User.findOne({$and:[{username:username}, {email:email}]}, function(err, found){
-    // TODO: beautify error message
     if(found){
-      res.send('<h2> Success! </h2> <p> A reset link sent to <strong>' + email + '</strong>. Please click the url to activate your new password </p> <meta http-equiv="refresh" content="3;url=./login" />');
+      // generate random password
+      // TODO: create separate middleware to handle this
+      const possible  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      let text;
+      function randomString(){
+        text = '';
+        for (let i=0; i<10; i+=1) {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
+      }
+      const reset_pass = randomString();
+      const link = randomString();
+
+      // TODO: programmatically change baseurl (localhost)
+      const reset_link = 'http://localhost:8000/account/resetpass/'+link;
+
+      // save to database
+      User.update({username:username},{$set:{reset_pass:reset_pass, reset_link:reset_link}}, function(err){
+        if(!err){
+          const title = 'reset pass';
+          const html  = '<p> Here is your new password <strong>'+ reset_pass + '</strong> Click here to reset : <a href="'+ reset_link+ '"> Reset </a> </p>';
+
+          // send email
+          const message = {
+              text: 'Admin League Cup',
+              from: 'Reset Password <winistein@gmail.com>',
+              to: '<'+email+'>',
+              subject: title,
+              attachment: [
+                  { data:html, alternative:true }
+              ]
+          };
+          server.send(message, (err, message) => {
+              console.log(err || message);
+          });
+          // TODO: beautify error message
+          // redirect to login page
+          res.send('<h2> Success! </h2> <p> A reset link sent to <strong>' + email + '</strong>. Please click the url to activate your new password </p> <meta http-equiv="refresh" content="3;url=./login" />');
+        }
+      })
     } else {
       res.send('<h3> Username or email not found! </h3> <p> Redirecting to login page... </p> <meta http-equiv="refresh" content="3;url=./login" />');
     }
   })
 })
 
+// handle reset link
+router.get("/account/resetpass/:link", function(req, res){
+  const link = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+  // check if link is match
+  User.findOne({reset_link:link},function(err, found){
+    if(found){
+      // change hash+salt password based on reset_pass
+      // 
+    } else {
+      res.send('Sorry, your link is invalid');
+    }
+  })
+})
+
 //handle sign up logic
 router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username, email: req.body.email});
+    var newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      reset_link: "",
+      reset_pass: ""
+    });
     User.register(newUser, req.body.password, function(err, user){
         if(err){
             console.log(err);
