4bf05c38fa74ae588bd2a98f0cd52c252b2bcbca
Ariana Isabel Hlavaty <arianahl@uk.ibm.com>
2017-10-06 10:52:57 +0100

    Make email to be unique, required and non-duplicated validation

--- models/user.js
+++ models/user.js
@@ -1,12 +1,34 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
+const userModelName = "User";
+
 var UserSchema = new mongoose.Schema({
-    username: String,
-    email:    String,
+    username: {
+        type: String,
+        required: true,
+        lowercase: false,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
     password: String
 });
 
-UserSchema.plugin(passportLocalMongoose);
+UserSchema.pre('save', function (next) {
+    var self = this;
+    mongoose.models[userModelName].find({$or: [{username: self.username}, {email: self.email}]}, function (err, docs) {
+        if (docs.length === 0) {
+            next();
+        } else {
+            next(new Error("Username or email already exists!"));
+        }
+    });
+});
 
-module.exports = mongoose.model("User", UserSchema);
+
+module.exports = mongoose.model(userModelName, UserSchema);
